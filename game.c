1519b9221bbc43b45398951668a5eb38ab42e0b7
sky-09-dev <sky564274@protonmail.com>
2025-11-21 21:24:13 -0600

    Update game.c

--- game.c
+++ game.c
@@ -26,7 +26,7 @@ printf("*                               *\n");
 printf("*                               *\n");
 printf("*********************************\n");
 
-sleep(5);
+sleep(4);
 
 printf("\033c");
 
@@ -59,12 +59,12 @@ printf("The mystours Sky island has shown up out of nowhere. \n");
 
 printf("\033[1A");
 printf("\033[K");
-sleep(8);
+sleep(6);
 printf("The island is called genesis floats in the sky with hundreds of chains to the ground.\n");
 
 printf("\033[1A");
 printf("\033[K");
-sleep(9);
+sleep(7);
 printf("Genesis came out of nowhere, and brought the monsters here, We must destroy all the chains and set the earth free. ");
 
 return 0;

1f12c30c33f9f26702cad5a221c4b29840d16305
sky-09-dev <sky564274@protonmail.com>
2025-11-21 21:12:30 -0600

    Add files via upload

---
+++ game.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main (){
+	
+
+char name [30] ="";
+
+
+printf("\033c");
+printf("what is your name\n");
+scanf(" %s", name);
+printf(" your name is: %s", name);
+
+printf("\033c");
+printf("*********************************\n");
+printf("*                               *\n");
+printf("*                               *\n");
+printf("*             Welcome           *\n");
+printf("*               To              *\n");
+printf("*             Skyland           *\n");
+printf("*                               *\n");
+printf("*                               *\n");
+printf("*                               *\n");
+printf("*                               *\n");
+printf("*********************************\n");
+
+sleep(5);
+
+printf("\033c");
+
+printf("---------------------------------------------------------------------------\n\n\n\n\n");
+
+
+printf("  o                                                                 ^ ^\n");
+printf(" -|-                                                                 > \n ");
+printf(" |\n");
+printf(" / \\\n");
+
+
+printf("---------------------------------------------------------------------------\n");
+
+
+
+printf("----------------\n");
+printf("|              |\n");
+printf("|  %s        |\n", name);
+printf("----------------\n");
+printf("|              |\n");
+printf("|  health: 25  |\n");
+printf("|  Magic: 10   |\n");
+printf("----------------\n");
+
+
+
+
+printf("The mystours Sky island has shown up out of nowhere. \n");
+
+printf("\033[1A");
+printf("\033[K");
+sleep(8);
+printf("The island is called genesis floats in the sky with hundreds of chains to the ground.\n");
+
+printf("\033[1A");
+printf("\033[K");
+sleep(9);
+printf("Genesis came out of nowhere, and brought the monsters here, We must destroy all the chains and set the earth free. ");
+
+return 0;
+}
